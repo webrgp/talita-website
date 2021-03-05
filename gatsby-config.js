c707251ee776909951bf563f7f330a1c5f2c26f4
Rodrigo Passos <rodrigo@hellodative.com>
2021-03-05 12:57:59 -0500

    add ts config

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,10 +26,10 @@ module.exports = {
         background_color: `#B41733`,
         theme_color: `#B41733`,
         display: `minimal-ui`,
-        icon: `src/assets/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/assets/images/favicon.png`, // This path is relative to the root of the site.
       },
     },
-    `gatsby-plugin-gatsby-cloud`,
+    // `gatsby-plugin-gatsby-cloud`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
