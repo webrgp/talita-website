defd881112b804dcefb5e14852bc1f57c0cd5ef2
Rodrigo Passos <rodrigo@hellodative.com>
2021-03-05 22:43:09 -0500

    add form to contact page

--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -9,10 +9,36 @@ const ContactPage = () => (
     <SEO title="Contact Us" />
     <PageHeader title="Contact Us" />
 
-    <div className="container">
-      <div className="card">
-        <div className="card-body text-center">
-          FORM
+    <div className="container-fluid container-lg">
+      <div className="row justify-content-center">
+        <div className="col col-md-10">
+          <fieldset>
+            <form>
+              <div className="form-row">
+                <div className="form-group col-md-12">
+                  <label htmlFor="name">Name</label>
+                  <input type="text" className="form-control" id="name" />
+                </div>
+              </div>
+              <div className="form-row">
+                <div className="form-group col-md-6">
+                  <label htmlFor="email">Email</label>
+                  <input type="email" className="form-control" id="email" />
+                </div>
+                <div className="form-group col-md-6">
+                  <label htmlFor="phone">Phone</label>
+                  <input type="tel" className="form-control" id="phone" />
+                </div>
+              </div>
+              <div className="form-row">
+                <div className="form-group col-md-12">
+                  <label htmlFor="message">Message</label>
+                  <textarea rows={5} className="form-control" id="message" />
+                </div>
+              </div>
+              <button type="submit" className="btn btn-red">Submit</button>
+            </form>
+          </fieldset>
         </div>
       </div>
     </div>
