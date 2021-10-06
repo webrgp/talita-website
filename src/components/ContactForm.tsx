1dc6067bf0c382a121efbb4dd73f3a774fabbfbe
Rodrigo Passos <rodrigo@hellodative.com>
2021-10-06 10:13:26 -0400

    add react-hook-form lib

--- src/components/ContactForm.tsx
+++ src/components/ContactForm.tsx
@@ -1,34 +1,67 @@
-import React from 'react'
+import React from "react"
+import { useForm, SubmitHandler } from "react-hook-form"
 
 import "../assets/styles/ContactForm.scss"
 
+type Inputs = {
+  name: string
+  email: string
+  phone: string
+  message: string
+}
+
 const ContactForm: React.FC = () => {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm<Inputs>()
+
+  const onSubmit: SubmitHandler<Inputs> = data => console.log(data)
+
+  console.log(errors)
+
   return (
     <fieldset className="ContactForm">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-row">
           <div className="form-group col-md-12">
             <label htmlFor="name">Name</label>
-            <input type="text" className="form-control" id="name" />
+            <input
+              type="text"
+              className="form-control"
+              {...register("name", { required: true })}
+            />
           </div>
         </div>
         <div className="form-row">
           <div className="form-group col-md-6">
             <label htmlFor="email">Email</label>
-            <input type="email" className="form-control" id="email" />
+            <input
+              type="email"
+              className="form-control"
+              {...register("email")}
+            />
           </div>
           <div className="form-group col-md-6">
             <label htmlFor="phone">Phone</label>
-            <input type="tel" className="form-control" id="phone" />
+            <input type="tel" className="form-control" {...register("phone")} />
           </div>
         </div>
         <div className="form-row">
           <div className="form-group col-md-12">
             <label htmlFor="message">Message</label>
-            <textarea rows={5} className="form-control" id="message" />
+            <textarea
+              rows={5}
+              className="form-control"
+              {...register("message")}
+            />
           </div>
         </div>
-        <button type="submit" className="btn btn-red">Submit</button>
+        <button type="submit" className="btn btn-red">
+          Submit
+        </button>
       </form>
     </fieldset>
   )
